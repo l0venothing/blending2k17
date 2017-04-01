f30b7b9d0b7a19967ac8e140ba0b5dd3028b514a
Thomas Stassin <thomas.stassin@gsmproductions.com>
2017-04-01 15:57:08 +0200

    map manager first draft

---
+++ blending_game_jam/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour {
+
+    public GameObject parent;
+    public GameObject destination;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ blending_game_jam/Assets/Scripts/GenerateRoom.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateRoom : MonoBehaviour {
+
+    public string textureName;
+    public int length = 3;
+    public List<int> doors;
+
+
+	// Use this for initialization
+	public void Initialize () {
+        for(int index=0; index<length; index++){
+            Room(index);
+        }
+	}
+
+    void Room(int number){
+
+        //compute position
+        Vector3 pos = new Vector3(0, 0, 0);
+        pos.x = number * 6f;
+
+
+        // load game object
+        GameObject bg_gameobject;
+
+        if(doors.Contains(number)){
+            bg_gameobject = Instantiate(Resources.Load("room_door")) as GameObject;
+            bg_gameobject.GetComponent<DoorManager>().parent = gameObject;
+        }
+        else{
+            bg_gameobject = Instantiate(Resources.Load("room_bg")) as GameObject;
+        }
+
+        bg_gameobject.transform.parent = transform;
+
+        // find good bg
+        GameObject border = Instantiate(Resources.Load("room_bg")) as GameObject;
+        border.transform.parent = transform;
+        border.name = "border_" + number;
+
+        // load good sprites for bg
+        string name = "room_bg_";
+        if(doors.Contains(number)){
+            name += "door_";
+        }
+        Sprite bgSprite;
+        bgSprite = Resources.Load<Sprite>(name + textureName);
+
+
+        Sprite borderSprite;
+        if(number == 0){
+            borderSprite = Resources.Load<Sprite>("border_left");
+        }
+        else if(number == length -1){
+            borderSprite = Resources.Load<Sprite>("border_right");
+        }
+        else{
+            borderSprite = Resources.Load<Sprite>("border_middle");
+        }
+
+        // make good sprite in game object
+        SpriteRenderer spriteRender = bg_gameobject.GetComponent<SpriteRenderer>();
+        spriteRender.sprite = bgSprite;
+        spriteRender.sortingOrder = -1;
+
+
+        SpriteRenderer spriteBorderRender = border.GetComponent<SpriteRenderer>();
+        spriteBorderRender.sprite = borderSprite;
+        spriteBorderRender.sortingOrder = 1;
+
+        //place object
+        bg_gameobject.transform.localPosition = pos;
+        border.transform.localPosition = pos;
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
