4e3052ed6d3327b7898944585eeaa20f5c7b144f
shadelbereth <scallakim@hotmail.com>
2017-04-01 12:01:54 +0200

    fight system ennemy script

---
+++ blending_game_jam/Assets/Scripts/Fight/Ennemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemy : MonoBehaviour {
+
+    [SerializeField]
+    float m_maxHitpoint;
+    [SerializeField]
+    float m_damage;
+
+    float m_hitpoint;
+
+	public void TakeDamage (float damage)
+    {
+        m_hitpoint -= damage;
+        if (m_hitpoint <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected void Die ()
+    {
+        // dying stuff
+    }
+}
