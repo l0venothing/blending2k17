2dc7bf862292b9ae61bcc55bcf0b48dcff740205
shadelbereth <scallakim@hotmail.com>
2017-04-01 12:00:46 +0200

    inventory item script

---
+++ blending_game_jam/Assets/Scripts/Fight/InventoryItem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : MonoBehaviour {
+
+    [SerializeField]
+    string m_name;
+    [SerializeField]
+    float m_damage;
+    [SerializeField]
+    float m_probabilityToHave;
+    [SerializeField]
+    List<Ennemy> m_vulnerableEnnemies;
+    [SerializeField]
+    float m_vulnerabilityEfficiency;
+    [SerializeField]
+    List<Ennemy> m_resistantEnnemies;
+    [SerializeField]
+    float m_resistanceEfficiency;
+
+    public float GetProbability ()
+    {
+        return m_probabilityToHave;
+    }
+
+    public void Use ()
+    {
+        float damage = m_damage;
+        Ennemy ennemy = InventoryManager.singleton.actualEnnemy;
+        if (m_vulnerableEnnemies.Contains(ennemy))
+        {
+            damage *= m_vulnerabilityEfficiency;
+        }
+        else if (m_resistantEnnemies.Contains(ennemy))
+        {
+            damage -= m_damage * m_resistanceEfficiency;
+        }
+        ennemy.TakeDamage(damage);
+    }
+}
