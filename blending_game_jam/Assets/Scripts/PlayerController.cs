22f2e85f4f555c13ef65c5c34465d971e58c3da2
Esther Roux <esther.roux13@gmail.com>
2017-04-01 17:16:34 +0200

    cam block

--- blending_game_jam/Assets/Scripts/PlayerController.cs
+++ blending_game_jam/Assets/Scripts/PlayerController.cs
@@ -12,17 +12,14 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
          translation= Time.deltaTime * speed;
         if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
         {
-            //  right
-            transform.Translate(translation, 0, 0);
+            transform.Translate(-translation, 0, 0);
         }
         else if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
         {
-            // left
-            transform.Translate(-translation, 0, 0);
+            transform.Translate(translation, 0, 0);
         }
     }
 }
