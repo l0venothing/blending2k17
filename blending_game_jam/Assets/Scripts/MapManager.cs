631ba2b4a0633220c6527452b6737ffbc9ba62a8
Thomas Stassin <thomas.stassin@gsmproductions.com>
2017-04-01 17:11:15 +0200

    doorManager first draft

--- blending_game_jam/Assets/Scripts/MapManager.cs
+++ blending_game_jam/Assets/Scripts/MapManager.cs
@@ -8,9 +8,16 @@ public class MapManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+        int numberOfDoor = System.Math.Min(Random.Range(2, 7), rooms);
+        int length = numberOfDoor * 3 + Random.Range(0,5);
+
         List<int> doors = new List<int>();
-        doors.Add(3);
-        NewRoom(new Vector3(0,0,0), doors, 4);
+        doors.Add(2);
+
+        int doorIndex = 0;
+
+        GameObject exit = NewRoom(new Vector3(0,0,0), "ground_floor", doors, length);
 	}
 
 	// Update is called once per frame
@@ -18,12 +25,17 @@ public class MapManager : MonoBehaviour {
 
 	}
 
-    void NewRoom(Vector3 position, List<int> doors, int length, GameObject origin=null){
+    private GameObject NewRoom(Vector3 position, string name, List<int> doors, int length, GameObject origin=null, bool outNeeded=true){
+
         GameObject room = Instantiate(Resources.Load("room")) as GameObject;
         room.transform.localPosition = position;
         GenerateRoom generate = room.GetComponent<GenerateRoom>();
+
         generate.length = length;
         generate.doors = doors;
-        generate.Initialize();
+        generate.textureName = name;
+        generate.Initialize(origin);
+
+        return null;
     }
 }
