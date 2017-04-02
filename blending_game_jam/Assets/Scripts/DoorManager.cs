0002b556f604877ab803c27853a9e7f681931395
Thomas Stassin <thomas.stassin@gsmproductions.com>
2017-04-02 08:50:55 +0200

    ceci est un commit

--- blending_game_jam/Assets/Scripts/DoorManager.cs
+++ blending_game_jam/Assets/Scripts/DoorManager.cs
@@ -1,21 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorManager : MonoBehaviour {
- 
+
     public GameObject parent;
-    public GameObject destination;
+    public DoorManager destination;
     private bool PlayerInTrigger;
+    private GameObject player;
+    private bool doorOpening = false;
+    private bool fade_out = false;
+    private bool fade_in = false;
+    public AudioSource audio;
+
 
 	// Use this for initialization
 	void Start () {
         PlayerInTrigger = false;
+        audio = GetComponent<AudioSource>();
 	}
+
 
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.UpArrow))
+        print(doorOpening);
+        if(doorOpening)
+        {
+            if(fade_out){
+                GameObject image = GameObject.FindGameObjectWithTag("Fade");
+                Color col = image.GetComponent<Image>().color;
+                col.a += Time.deltaTime * 20;
+
+                if(col.a >= 1f){
+                    col.a = 1f;
+                    audio.Play();
+                    fade_out = false;
+                }
+                image.GetComponent<Image>().color = col;
+            }
+            else if(!audio.isPlaying && !fade_in){
+                fade_in = true;
+            }
+            else if(fade_in){
+                GameObject image = GameObject.FindGameObjectWithTag("Fade");
+                Color col = image.GetComponent<Image>().color;
+                col.a -= Time.deltaTime * 20;
+
+                if(col.a <= 0f){
+                    col.a = 0f;
+                    fade_in = false;
+                    doorOpening = false;
+                    // :D
+                    destination.parent.active = true;
+                    Vector3 pos = player.transform.position;
+                    pos.x =  destination.transform.position.x;
+                    player.transform.position = pos;
+                    parent.active = false;
+                }
+                image.GetComponent<Image>().color = col;
+            }
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
         {
             if (PlayerInTrigger)
             {
@@ -25,7 +71,9 @@ public class DoorManager : MonoBehaviour {
 	}
 
     public void OpenDoor(){
-        print("Door open!");
+        doorOpening = true;
+        fade_out = true;
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -33,6 +81,7 @@ public class DoorManager : MonoBehaviour {
        if (other.gameObject.tag == "Player")
         {
             PlayerInTrigger = true;
+            player = other.gameObject;
         }
     }
 
