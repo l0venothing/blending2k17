bedbc17d5e762dbb7ef80c6fcc05c6297a2ae949
Esther Roux <esther.roux13@gmail.com>
2017-04-02 13:10:49 +0200

    merge

---
+++ blending_game_jam/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+    public Text scoreText;
+    private int victimNumber;
+	// Use this for initialization
+	void Start () {
+        victimNumber = PlayerPrefs.GetInt("score");
+    }
+	
+	
+	void Update () {
+      
+        scoreText.text = "You killed : " + victimNumber + " Monsters";
+		
+	}
+}
