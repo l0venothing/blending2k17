8610b977d8508f5c2d23616ee5e3bb107088366a
Esther Roux <esther.roux13@gmail.com>
2017-04-08 16:24:33 +0200

    hero walk sound

--- blending_game_jam/Assets/Scripts/PlayerController.cs
+++ blending_game_jam/Assets/Scripts/PlayerController.cs
@@ -6,29 +6,51 @@ public class PlayerController : MonoBehaviour
 {
     public float speed =0.9f;
     float translation;
+    public AudioSource audiosource;
 
     public Animator animator;
     void Start()
     {
-
+        audiosource=GetComponent<AudioSource>();
     }
     void Update()
     {
          translation= Time.deltaTime * speed;
         if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
         {
+           
             transform.Translate(translation, 0, 0);
+            if (!audiosource.isPlaying)
+            {
+                audiosource.Play();
+            }
             animator.SetBool("iddle", false);
             GetComponent<SpriteRenderer>().flipX = false;
+          
         }
         else if (Input.GetButton("Horizontal") && Input.GetAxisRaw("Horizontal") < 0)
         {
+            
             transform.Translate(-translation, 0, 0);
+            if (!audiosource.isPlaying)
+            {
+                audiosource.Play();
+            }
+          
             animator.SetBool("iddle", false);
             GetComponent<SpriteRenderer>().flipX = true;
+           
+         
         }
         else{
             animator.SetBool("iddle", true);
+            if (audiosource.isPlaying)
+            {
+                audiosource.Stop();
+            }
+
+
+
         }
     }
 }
